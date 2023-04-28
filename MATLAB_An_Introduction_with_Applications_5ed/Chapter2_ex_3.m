1c71847adbae8c05c99f0c5f9a0e02a268db410e
Bujang <42134222+jhpower901@users.noreply.github.com>
2023-04-28 18:00:03 +0900

    Update Chapter2_ex_3.m

--- MATLAB_An_Introduction_with_Applications_5ed/Chapter2_ex_3.m
+++ MATLAB_An_Introduction_with_Applications_5ed/Chapter2_ex_3.m
@@ -1,5 +1,7 @@
-A = [linspace(2,6,5)' linspace(5,9,5)' linspace(11,12,5)' linspace(14, 18,5)' linspace(17,21,5)']
+A = [linspace(2,6,5)' linspace(5,9,5)' linspace(11,15,5)' linspace(14, 18,5)' linspace(17,21,5)']
 B = [linspace(5,30, 6); linspace(30,55, 6); linspace(55,80, 6)]
 v = 99:-1:91
 
-A(1,3,3:6) = B(1:2, 1:4)
+A([1,3],3:6) = B(1:2,1:4)
+A(4,3:6) = v(5:8)
+A(5,3:6) = B(3,2:5)
